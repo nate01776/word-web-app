984e3cd7c337bd04bc936ca9a68760e7b0930150
Nathan W <nate01776@gmail.com>
2016-12-11 22:12:34 -0500

    added saved list and api call routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/saved" component={Saved} />
+    <Route path="/connect" component={ApiConnect}
+  </Router>
+  ), document.getElementById('root')
 );
