538c1f95c90aa4f571edf37dd444aa9dcd3767d4
Nathan W <nate01776@gmail.com>
2016-12-13 15:39:39 -0500

    added test word json file and jsonfile package

--- src/SearchList.js
+++ src/SearchList.js
@@ -5,6 +5,21 @@ const SearchList = props => {
   let queryResults = props.queryResults;
   let outputResults = "";
 
+  function handleSaveWord(word, type, def) {
+    let exportWord = word;
+    let exportType = type;
+    let exportDef = def;
+
+    fetch('/public/api/v1/saved-words.json', {
+      method: 'post',
+      body: JSON.stringify({
+        word: exportWord,
+        type: exportType,
+        defenition: exportDef
+      })
+    })
+  }
+
   if (queryResults.length !== 0) {
     let i = 0;
     outputResults = queryResults.map(query => {
@@ -14,12 +29,13 @@ const SearchList = props => {
 
       if (query.defenition !== null) {
         newDef = query.defenition;
-        newType = query.type + " - ";
+        newType = query.type;
       };
       i ++;
       return (
         <div key={key}>
-          {newType} {newDef}
+          {newType} - {newDef}
+          <button className="test" onClick={() => handleSaveWord({outputWord}, {newType}, {newDef})}>save</button>
         </div>
       )
     })
