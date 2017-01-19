69bc46c92031354fe9f01ae76f94ed93d01600be
Nathan W <nate01776@gmail.com>
2017-01-18 19:15:16 -0500

    reran create-react-app

--- server.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-require 'json'
-require 'httparty'
-
-get '/api/connect/:search_word' do
-  content_type 'application/json'
-  response = HTTParty.get('https://owlbot.info/api/v1/dictionary/' + params[:search_word] + '?format=json')
-  response.to_json
-end
